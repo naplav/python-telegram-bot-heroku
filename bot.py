a16d101936bb1b895032576d13b4cabf8a2e2044
naplav <fabrizio.mei1988@gmail.com>
2020-08-22 01:01:37 +0200

    first commit

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://obscure-river-18276.herokuapp.com/' + 1389566736:AAEzWx-Pmr6XKOsYNLwFLXQqAvujABiyfdQ)
+    updater.bot.setWebhook('https://intense-anchorage-51175.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
