93b2ba1e45efd4e78a31c0116d33b534eb83e8a2
naplav <fabrizio.mei1988@gmail.com>
2020-08-22 02:12:05 +0200

    test

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
+from selenium import webdriver
+import time
+
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
@@ -15,6 +18,21 @@ TOKEN = '1389566736:AAEzWx-Pmr6XKOsYNLwFLXQqAvujABiyfdQ'
 def start(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hi!')
+    driver = webdriver.Firefox()
+
+    driver.get('https://www.hurryupde.com//aproducts.php')
+    while True:
+       time.sleep(20)
+       driver.refresh()
+    driver.quit()
+	
+    web_content = get_webpage_content('https://www.hurryupde.com//aproducts.php') # get the page source
+    last_mod_time = str(get_last_modified_time(web_content)) # update when it was created last.
+    hurryup_df = get_worldometer_df(web_content) # create the dataframe
+    hurryup_df["mod_time"] = [last_mod_time]*hurryup_df.shape[0]
+
+    print hurryup_df
+
 
 #def help(update, context):
     """Send a message when the command /help is issued."""
@@ -52,7 +70,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://intense-anchorage-51175.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://agile-earth-81836.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
@@ -61,3 +79,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def repeat_hurryup(hurryup_URL):
+      web_content = get_webpage_content(hurryup_URL) # get the page source
+      last_mod_time = str(get_last_modified_time(web_content)) # update when it was created last.
+      hurryup_df = get_worldometer_df(web_content) # create the dataframe
+      hurryup_df["mod_time"] = [last_mod_time]*hurryup_df.shape[0]
